f3b64825e37c1d78b8ba0c9819fa4fc5c21f7d1f
Jan Švejda <jenda.svejda@gmail.com>
2019-04-24 10:00:49 +0200

    Typedef example

---
+++ testfiles/03.typedef.c
@@ -0,0 +1,12 @@
+extern void __VERIFIER_error();
+
+//typedef long int size_t;
+
+int main() {
+    size_t t = 1 + 9/2 + 3 * 10;
+    if (t == 35) {
+        __VERIFIER_error();
+        return 1;
+    }
+    return 0;
+}
