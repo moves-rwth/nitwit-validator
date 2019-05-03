9701f887fdbcfdb625d7bd9214f347f4f63adf8e
Jan Švejda <jenda.svejda@gmail.com>
2019-05-03 10:28:03 +0200

    Results for new version; analysis of differences b. results

--- test-bench/bench_results_analysis.py
+++ test-bench/bench_results_analysis.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 import json
+from typing import Tuple, List, Optional
 
 WITNESSES_BY_PROGRAM_HASH_DIR = "witnessListByProgramHashJSON"
 WITNESS_INFO_BY_WITNESS_HASH_DIR = "witnessInfoByHash"
@@ -26,25 +27,31 @@ def setup_dirs(dir: str) -> bool:
 	print("Directories found!")
 	return True
 
+def load_result_file(results: str) -> Optional[List[str]]:
+	if not (os.path.exists(results) and os.path.isfile(results)):
+		print("Cannot load output file with info about witnesses.")
+		return None
+	with open(results, 'r') as fp:
+		valid_jObj = json.load(fp)
+	return list(valid_jObj)
 
-def analyze_bench_output(results: str, name: str, search_string: str):
+def analyze_bench_output(results: List[str], name: str, search_string: str):
 	'''
 	Analyzes the producers and wrong results of the C Witness Validator
-	:param results: Path to the file with a json list of witness info files.
+	:param results: List of witness info files.
 	:param name: Type of output.
 	:param search_string: The string that has to occur in programfile field of the info file
 	:return:
 	'''
-	if not (os.path.exists(results) and os.path.isfile(results)):
-		print("Cannot load output file with info about witnesses.")
+
+	if results is None:
 		return
+
 	prod_map = {}
 	unknown = 0
 	false_positives = 0
-	with open(results, 'r') as fp:
-		valid_jObj = json.load(fp)
 
-	for w in valid_jObj:
+	for w in results:
 		with open(os.path.join(WITNESS_INFO_BY_WITNESS_HASH_DIR, w), 'r') as fp:
 			info_jObj = json.load(fp)
 			if 'producer' in info_jObj:
@@ -60,11 +67,17 @@ def analyze_bench_output(results: str, name: str, search_string: str):
 			if pf.find(search_string) == -1:
 				false_positives = false_positives + 1
 
-	print(prod_map)
 	print('-' * 40)
+	print(prod_map)
 	print(f"Unknown producers for {unknown} witnesses.")
-	print(f"Incorrect results for {name}: {false_positives}, i.e. {false_positives / len(valid_jObj) * 100}%.")
-	print(f"In total {name} {len(valid_jObj)}.")
+	fp_rate = 0 if len(results) == 0 else false_positives / len(results) * 100
+	print(f"Incorrect results for {name}: {false_positives}, i.e. {fp_rate}%.")
+	print(f"In total {name} {len(results)}.")
+
+
+def get_differences(a: List[str], b: List[str]) -> Tuple[List[str], List[str]]:
+	sa, sb = set(a), set(b)
+	return list(sa.difference(sb)), list(sb.difference(sa))
 
 
 def main():
@@ -72,24 +85,44 @@ def main():
 	parser.add_argument("-w", "--witnesses", required=True, type=str, help="The directory with unzipped witnesses.")
 	parser.add_argument("-v", "--validated", required=False, type=str, default=None,
 	                    help="The file with info about validated witnesses.")
+	parser.add_argument("-dv", "--dvalidated", required=False, type=str, default=None,
+	                    help="The file with info about validated witnesses of the second result.")
 	parser.add_argument("-nv", "--nonvalidated", required=False, type=str, default=None,
 	                    help="The file with info about non-validated witnesses.")
 	parser.add_argument("-bp", "--badlyparsed", required=False, type=str, default=None,
 	                    help="The file with info about badly parsed witnesses.")
+	parser.add_argument("-d", "--differences", required=False, action='store_true', help="Show the differences between two results.")
 	# parser.add_argument("-c", "--config", required=True, type=str, help="The verifier configuration file.")
 
 	args = parser.parse_args()
 	if not setup_dirs(args.witnesses):
 		return 1
 
-	if args.validated:
-		analyze_bench_output(args.validated, 'validated', '_false-unreach-call')
+	if not args.differences:
+		if args.validated:
+			analyze_bench_output(load_result_file(args.validated), 'validated', '_false-unreach-call')
 
-	if args.nonvalidated:
-		analyze_bench_output(args.nonvalidated, 'non-validated', '_true-unreach-call')
+		if args.nonvalidated:
+			analyze_bench_output(load_result_file(args.nonvalidated), 'non-validated', '_true-unreach-call')
 
-	if args.badlyparsed:
-		analyze_bench_output(args.badlyparsed, 'badly parsed', '')
+		if args.badlyparsed:
+			analyze_bench_output(load_result_file(args.badlyparsed), 'badly parsed', '')
+	else:
+		if args.validated and args.dvalidated:
+			not_in_d, not_in_v = get_differences(load_result_file(args.validated),
+			                                      load_result_file(args.dvalidated))
+			print(f"Witnesses not in {args.validated}: {len(not_in_v)}")
+			analyze_bench_output(not_in_v, 'validated by second, but not by first', '_false-unreach-call')
+			print('='*80)
+			print(f"Witnesses not in {args.dvalidated}: {len(not_in_d)}")
+			analyze_bench_output(not_in_d, 'validated by first, but not by second', '_false-unreach-call')
+			print('='*80)
+
+		# if args.nonvalidated:
+		# 	analyze_bench_output(args.nonvalidated, 'non-validated', '_true-unreach-call')
+		#
+		# if args.badlyparsed:
+		# 	analyze_bench_output(args.badlyparsed, 'badly parsed', '')
 
 
 if __name__ == "__main__":
