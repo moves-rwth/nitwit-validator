22216c9d0418bebfa24477ea16196efd5bb6366d
Jan Švejda <jenda.svejda@gmail.com>
2019-05-04 15:52:11 +0200

    Fix test

--- testfiles/04.gcc.attribute.c
+++ testfiles/04.gcc.attribute.c
@@ -1,15 +1,17 @@
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
 
 int f2(int n) {
-    if (n < 3) return;
+    if (n < 3) return n;
     f(--n);
     __VERIFIER_error();
+    return 0;
 }
 
 int f(int n) {
-    if (n < 3) return;
+    if (n < 3) return n;
     f2(--n);
     __VERIFIER_error();
+    return 0;
 }
 
 int main() {
