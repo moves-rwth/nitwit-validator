41be806b527d9810862e3e990f19d57a75801332
Jan Švejda <jenda.svejda@gmail.com>
2019-05-29 18:01:24 +0200

    Plotting validator comparison

--- bench/validator_analysis.py
+++ bench/validator_analysis.py
@@ -2,25 +2,88 @@ import argparse
 import os
 import sys
 import json
+from collections import namedtuple
 from typing import Tuple, List, Optional, Dict
+
+import math
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from common.utils import *
 
+sns.set()
 
-def analyze(results: Tuple[List, ...], validators: dict):
-	val, nval, bpar = results
-	by_witness = validators['byWitnessHash'].keys()
-	witness_keys = set([hash['witnessSHA'].lower() for key in by_witness for hash in validators['byWitnessHash'][key]['results']])
+STATUSES = {
+	'false(unreach-call)': 0,
+	'unknown': 1,
+	'true': 2,
+	'timeout': 3,
+	'timeout (true)': 3,
+	'timeout (error (7))': 3,
+	'timeout (false(unreach-call))': 3,
+	'exception': 4,
+	'error (1)': 4,
+	'error (2)': 4,
+	'error (7)': 4,
+	'error (invalid witness file)': 4,
+	'assertion': 4,
+	'out of memory': 5,
+	'-': 6
+}
+col_names = ['false', 'unknown', 'true', 't/o', 'error', 'o/m', 'not run']
 
-	found = 0
-	all = val + nval + bpar
-	for r in all:
-		if str(r[1]).partition('.json')[0].lower() in witness_keys:
-			found = found + 1
-		# else:
-		# 	print(r)
-		# 	break
-	print(f"I could match {found} out of {len(all)} witnesses")
+RESULT_CODES = {
+	0: 0,  # false
+	4: 1,  # parse error
+	246: 1,  #
+	5: 2,  # error not reached
+	9: 3,  # timeout
+	241: 4,  # error, witness in sink
+	242: 5,  # program finished, witness not in violation state
+}
+row_names = ['false', 'unknown', 'not reached', 't/o', 'sink', 'not in violation state']
+
+
+def get_matching(all_results: List, validators: dict) -> Dict[str, int]:
+	by_witness = validators.keys()
+	# witness_keys = [hash['witnessSHA'].lower() for key in by_witness for hash in validators[key]['results']]
+	witness_keys = set(by_witness)
+
+	matched = list(filter(lambda r: str(r[1]).partition('.json')[0] in witness_keys, all_results))
+	print(f"I could match {len(matched)} out of {len(all_results)} witnesses")
+	return dict([(r[1].partition('.json')[0], r[0]) for r in matched])
+
+
+# missing = {}
+# else:
+# 	with open(os.path.join('../../data/sv-witnesses/witnessInfoByHash', r[1])) as fp:
+# 		p = json.load(fp)['producer']
+# 		if p == 'CSeq':
+# 			print(r[1])
+# 			break
+# 		emplace_in_dict(missing, p)
+# print(missing)
+
+
+def analyze(matching: Dict[str, int], validators: dict):
+	print(f"Analyze {len(matching)}")
+	fig, axs = plt.subplots(nrows=2, ncols=2)
+	for i in range(4):
+		# comparison = {}
+		name = None
+		heatmap = np.zeros((6, 7), dtype=int)
+		for w, c in matching.items():
+			if name is None:
+				name = validators[w]['results'][i]['tool']
+			heatmap[RESULT_CODES[c], STATUSES[validators[w]['results'][i]['status']]] += 1
+		dataframe = pd.DataFrame(data=heatmap, index=row_names, columns=col_names)
+		ax = sns.heatmap(dataframe, annot=True, fmt="d", ax=axs[math.floor(i / 2)][i % 2], cmap="BuGn")
+		ax.set_title(name)
+
+	# print(f"Comparison with {name}:\n\t{comparison}\n")
+	plt.show()
 
 
 def main():
@@ -33,11 +96,15 @@ def main():
 	args = parser.parse_args()
 
 	results = load_result_files(args.results)
+	val, nval, bpar = results
+	all = val + nval + bpar
+
 	validators = load_validators_result_file(args.validators)
 	if results is None or validators is None:
 		return 1
 
-	analyze(results, validators)
+	matching = get_matching(all, validators['byWitnessHash'])
+	analyze(matching, validators['byWitnessHash'], )
 
 
 if __name__ == "__main__":
