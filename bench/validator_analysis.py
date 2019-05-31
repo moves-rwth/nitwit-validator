4c91088eab1f1b67338ba3b701286354726b71ab
Jan Švejda <jenda.svejda@gmail.com>
2019-05-31 11:05:54 +0200

    Add config for running matched files

--- bench/validator_analysis.py
+++ bench/validator_analysis.py
@@ -1,15 +1,10 @@
 import argparse
-import os
-import sys
-import json
-from collections import namedtuple
-from typing import Tuple, List, Optional, Dict
 
 import math
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
 
 from common.utils import *
 
@@ -38,6 +33,7 @@ RESULT_CODES = {
 	0: 0,  # false
 	4: 1,  # parse error
 	246: 1,  #
+	244: 1,  # identifier undefined
 	5: 2,  # error not reached
 	9: 3,  # timeout
 	241: 4,  # error, witness in sink
@@ -46,32 +42,23 @@ RESULT_CODES = {
 row_names = ['false', 'unknown', 'not reached', 't/o', 'sink', 'not in violation state']
 
 
-def get_matching(all_results: List, validators: dict) -> Dict[str, int]:
+def get_matching(all_results: List, validators: dict, outputmatched: str = None) -> Dict[str, int]:
 	by_witness = validators.keys()
 	# witness_keys = [hash['witnessSHA'].lower() for key in by_witness for hash in validators[key]['results']]
 	witness_keys = set(by_witness)
 
 	matched = list(filter(lambda r: str(r[1]).partition('.json')[0] in witness_keys, all_results))
+	if outputmatched is not None:
+		with open(outputmatched, 'w') as fp:
+			json.dump(matched, fp)
 	print(f"I could match {len(matched)} out of {len(all_results)} witnesses")
 	return dict([(r[1].partition('.json')[0], r[0]) for r in matched])
-
-
-# missing = {}
-# else:
-# 	with open(os.path.join('../../data/sv-witnesses/witnessInfoByHash', r[1])) as fp:
-# 		p = json.load(fp)['producer']
-# 		if p == 'CSeq':
-# 			print(r[1])
-# 			break
-# 		emplace_in_dict(missing, p)
-# print(missing)
 
 
 def analyze(matching: Dict[str, int], validators: dict):
 	print(f"Analyze {len(matching)}")
 	fig, axs = plt.subplots(nrows=2, ncols=2)
 	for i in range(4):
-		# comparison = {}
 		name = None
 		heatmap = np.zeros((6, 7), dtype=int)
 		for w, c in matching.items():
@@ -82,7 +69,6 @@ def analyze(matching: Dict[str, int], validators: dict):
 		ax = sns.heatmap(dataframe, annot=True, fmt="d", ax=axs[math.floor(i / 2)][i % 2], cmap="BuGn")
 		ax.set_title(name)
 
-	# print(f"Comparison with {name}:\n\t{comparison}\n")
 	plt.show()
 
 
@@ -92,6 +78,7 @@ def main():
 	                    help="The JSON file with results about SV-COMP validator runs.")
 	parser.add_argument("-r", "--results", required=True, type=str,
 	                    help="The directory with validation results of CWValidator.")
+	parser.add_argument("-om", "--outputmatched", required=False, type=str, help="File where to write the matched files config.")
 
 	args = parser.parse_args()
 
@@ -103,8 +90,8 @@ def main():
 	if results is None or validators is None:
 		return 1
 
-	matching = get_matching(all, validators['byWitnessHash'])
-	analyze(matching, validators['byWitnessHash'], )
+	matching = get_matching(all, validators['byWitnessHash'], args.outputmatched)
+	# analyze(matching, validators['byWitnessHash'])
 
 
 if __name__ == "__main__":
