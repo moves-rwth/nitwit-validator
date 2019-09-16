1d0e88c7d8eee9709ebabc25b1e28625b0338c52
Jan Švejda <jenda.svejda@gmail.com>
2019-09-16 17:01:55 +0200

    Improve scripts; rename bench defs

--- bench/svcomp/cwvalidator.py
+++ bench/svcomp/cwvalidator.py
@@ -38,14 +38,14 @@ class Tool(benchexec.tools.template.BaseTool):
 	Tool info for {TODO CWValidator}, an interpreter-based violation witness validator.
 	URL:
 	"""
+	REQUIRED_PATHS = []
 	BUILD_PATH = 'cmake-build-release'
 
 	def executable(self):
-		executable = util.find_executable('validator.sh')
-		build_path = os.path.join(os.path.pardir(executable), self.BUILD_PATH)
-		if not os.path.isdir(build_path):
-			self._build(build_path)
-		if not os.path.isfile(os.path.join(build_path, "cwvalidator32")) or \
+		executable = util.find_executable('validate.sh')
+		build_path = os.path.join(os.path.dirname(executable), self.BUILD_PATH)
+		if not os.path.isdir(build_path) or \
+				not os.path.isfile(os.path.join(build_path, "cwvalidator32")) or \
 				not os.path.isfile(os.path.join(build_path, "cwvalidator64")):
 			logging.warning("Required binary files for CWValidator not found in {0}.".format(build_path))
 		return executable
@@ -53,13 +53,6 @@ class Tool(benchexec.tools.template.BaseTool):
 	def program_files(self, executable):
 		return [executable, os.path.join(self.BUILD_PATH, 'cwvalidator32'),
 		        os.path.join(self.BUILD_PATH, 'cwvalidator64')]
-
-	def _build(self, executableDir):
-		logging.debug('Building CWValidator in directory {0}.'.format(executableDir))
-		build = subprocess.Popen(['build.sh'])
-		build.communicate()
-		if build.returncode:
-			sys.exit('Failed to build cwvalidator, fix it please.')
 
 	def version(self, executable):
 		stdout = self._version_from_tool(executable, '--version')
@@ -71,24 +64,23 @@ class Tool(benchexec.tools.template.BaseTool):
 	def cmdline(self, executable, options, tasks, propertyfile=None, rlimits={}):
 		return [executable] + options + tasks
 
-	def determine_result(self, ret_code, returnsignal, output, isTimeout):
+	def determine_result(self, returncode, returnsignal, output, isTimeout):
 		"""
 		@param returncode: code returned
 		@param returnsignal: signal, which terminated validator
 		@param output: the std output
 		@return: status of validator after executing a run
 		"""
-
-		if ret_code == 0 or ret_code == 245:
+		if returnsignal == 0 and (returncode == 0 or returncode == 245):
 			status = result.RESULT_FALSE_REACH
-		elif ret_code is None or ret_code == -9:
+		elif returncode is None or returncode == -9:
 			status = 'TIMEOUT'
-		elif ret_code in [4, 5, 240, 241, 242, 243, 250]:
+		elif returncode in [4, 5, 240, 241, 242, 243, 250]:
 			status = result.RESULT_UNKNOWN
 		else:
 			status = result.RESULT_ERROR
 
-		if 'Out of memory' in output:
+		if 'Out of memory' in output or returncode == 251:
 			status = 'OUT OF MEMORY'
 
 		if not status:
--- validate.sh
+++ validate.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
 # Usage: ./validate.sh -v/--version
-#        ./validate.sh <witness> <program>
-#        ./validate.sh -64 <witness> <program>
-
-if [[ ! -d "cmake-build-release" ]]
-then
-  echo "Build the tool in cmake-build-release first."
-fi
+#        ./validate.sh -w/--witness <witness> <program>
+#        ./validate.sh -64 -w/--witness <witness> <program>
 
 if [[ $1 == "-v" || $1 == "--version" ]]
 then
   git rev-parse --short HEAD && exit 0
 fi
 
-if [[ $1 == "-64" && -e $2 && -e $3 ]]
+if [[ ! -d "cmake-build-release" ]]
 then
-  ./cmake-build-release/cwvalidator64 $2 $3
-elif [[ -e $1 && -e $2 ]]
+  echo "Build the tool in cmake-build-release first."
+  exit 255
+fi
+
+if [[ $1 == "-64" && ( $2 == "-w" || $2 == "--witness") && -e $3 && -e $4 ]]
 then
-  ./cmake-build-release/cwvalidator32 $1 $2
+  ./cmake-build-release/cwvalidator64 $3 $4
+elif [[ ( $1 == "-w" || $1 == "--witness") && -e $2 && -e $3 ]]
+then
+  ./cmake-build-release/cwvalidator32 $2 $3
 fi
