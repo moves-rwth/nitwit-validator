15c3dd026791b8f2e2d0e0fc1b8c710da77f2ae4
Jan Švejda <jenda.svejda@gmail.com>
2020-02-02 19:31:56 +0100

    Tests

---
+++ testfiles/25.branch.c
@@ -0,0 +1,10 @@
+void __VERIFIER_error();
+int __VERIFIER_nondet_int(void);
+
+int main ()
+{
+    int i = __VERIFIER_nondet_int();
+    if (i == 7 || i == 27){
+        __VERIFIER_error();
+    }
+}
