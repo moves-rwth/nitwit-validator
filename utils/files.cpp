7d989170465f64b3ed657f288560b4bd3567e3b5
Berger <berger@cs.rwth-aachen.de>
2022-09-17 12:39:28 +0200

    Implemented a search/replace filter for the wacky __extension__ based stuff we do not understand.

--- utils/files.cpp
+++ utils/files.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "files.hpp"
+#include <algorithm>
 #include <cerrno>
 #include <cstring>
 
@@ -10,6 +11,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <regex>
 #include <sstream>
 
 std::string readFile(const char *FileName, bool& error) {
@@ -26,5 +28,22 @@ std::string readFile(const char *FileName, bool& error) {
     std::ifstream inStream(FileName);
     std::stringstream buffer;
     buffer << inStream.rdbuf();
-    return buffer.str();
+
+    // Filter wacky stuff from the input...
+    std::string result = buffer.str();
+    std::regex re(R"((void\s+reach_error\(\)\s*\{\s*\(\s*\(\s*void\s*\)\s*sizeof\s*\(\s*\(\s*0\s*\)\s*\?\s*1\s*:\s*0\)\s*,\s*__extension__\s*\(\s*\{\s*if\s*\(\s*0\s*\)\s*;\s*else\s+__assert_fail\s*\(\s*"\d+"\s*,\s*"[^"]*"\s*,\s*\d+\s*,\s*__extension__\s+__PRETTY_FUNCTION__\s*\);\s*\}\)\s*\);\s*\}))");
+    std::smatch match;
+    if (std::regex_search(result, match, re) && match.size() > 1) {
+        // Count the number of newlines in the match
+        std::string const matchText = match.str(1);
+        auto const numNewlines = std::count(matchText.begin(), matchText.end(), '\n');
+        std::string replacementFunction = R"(void reach_error() { abort(); )" + std::string(numNewlines, '\n') + R"(})";
+        result = std::regex_replace(result, re, replacementFunction);
+        std::cerr << "Warning: Replaced wacky reach_error function definition containing ugly __extension__, offsets might be a little off!" << std::endl;
+    }
+    else {
+        std::cout << "No changes done." << std::endl;
+    }
+
+    return result;
 }
